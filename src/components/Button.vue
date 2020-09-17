3efa4e57486452b40ff869054f26bb3e36d2dccb
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-09-17 23:55:47 +0530

    [Akshay]:ADDED:New Components

---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="bu-div">
+        
+        CHOOSE TYPE
+
+    </div>
+</template>
+
+<script>
+export default {
+  name:'Button'
+  
+}
+</script>
+
+<style scoped>
+.bu-div{
+    text-align: center;
+    margin-top: 5%;
+    padding-right: 20%;
+    background-color: #F5F6FA;
+}
+</style>
---
+++ src/components/TextBox.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="txdiv">
+    <div class="ft">
+       <a>FROM</a>
+        <a id="to">TO</a> 
+    </div>
+    
+    <div class="ft2">   
+        <input type="text" id="first">    
+        <input type="text" id="second">
+    </div>
+</div>
+
+</template>
+
+<script>
+export default {
+    name:'TextBox'
+}
+</script>
+
+<style scoped>
+#first{
+    
+    padding-top: 1%;
+    padding-bottom: 1%;
+    padding-left: 5%;
+}
+#second{
+    
+    margin-left: 20%;
+    padding-top: 1%;
+    padding-bottom: 1%;
+    padding-left: 5%;
+}
+#to{
+    margin-left: 40%;
+}
+.ft,.ft2{
+    display: flex;
+    justify-content:flex-start;
+}
+.txdiv{
+    margin-top:15%;
+    padding-left: 30%;
+}
+
+
+</style>
