8b3910036a825072b8dc80bb319d77ebcdc56501
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-09-17 14:46:04 +0530

    [Akshay]:Initialised The Project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,128 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <AppBar/>
+    <MediaCard/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import MediaCard from './components/MediaCard';
+import AppBar from './components/AppBar';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data(){ 
+    return{
+    title:'Welcome To Quantity Measurement'
+
+    }
+  },components:{
+    MediaCard,AppBar
+
+    }
+    }
+  
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.tdiv{
+  padding-top: 1%;
+  padding-bottom: 1%;
+  text-align: center;  
+  background-color:#446Dff ;
+
 }
+#ti{
+font-size:x-large;
+color:white;
+}
+.main2{
+  background-color: #F5F6FA;
+  height: 500px;
+}
+.buttondiv{
+  text-align: center;
+  padding-top: 5%;
+  padding-right: 37%;
+  background-color:#F5F6FA ;
+  }
+#chbutton{
+  background-color:#F5F6FA ;
+  border: white;
+  
+
+}
+.img{
+  width:180px; 
+  height:160px;
+
+}
+/* .box{
+  display:flex;
+  justify-content: center;
+} */
+.container { 
+      display:inline-flex;
+      /* width:100%; */
+			max-height: 250px;
+			background-color:#F5F6FA;
+      margin-top: 1%; 
+			/* padding-top:5%;  */
+			padding-left:20%; 
+			/* padding-right:27%;  */
+      /* padding-bottom: 2%; */
+		} 
+		#st-box { 
+			float:left; 
+			width:180px; 
+			height:160px; 
+			background-color:white;
+      border-radius: 5%;
+      margin-left: 1%;
+      margin-right: 1%;
+ 
+		} 
+    #st-box:hover{
+      background-color:#EDFDF9;
+      border:solid #446Dff;
+    }
+    #nd-box { 
+			float:left; 
+			width:180px; 
+			height:160px; 
+			background-color:white;
+      border-radius: 5%;
+      margin-left:1%;
+      margin-right:1%;;
+ 
+		}
+    #nd-box:hover{
+      background-color:#EDFDF9;
+      border:solid #446Dff;
+    }
+    #rd-box { 
+			float:right; 
+			width:180px; 
+			height:160px; 
+			background-color:white;
+      border-radius: 5%;
+      margin-left: 1%;
+      margin-right: 1%;
+		}
+    #rd-box:hover{
+      background-color:#EDFDF9;
+      border:solid #446Dff;
+    }
+.ft{
+  margin-top: 1%;
+  display: flex;
+  justify-content:space-around;
+
+}
+ #fr{
+  margin-left: 24%;
+}
+#to{
+  margin-right: 25%;
+  }
 </style>
---
+++ src/components/AppBar.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+
+   <md-toolbar class="md-primary">
+      <h3 class="md-title">Primary</h3>
+    </md-toolbar>
+</div>
+</template>
+
+<script>
+// import { component } from 'vue/types/umd'
+
+export default {
+   name: 'AppBar'
+
+ }
+
+  
+</script>
+
+<style scoped>
+  .md-toolbar + .md-toolbar {
+    margin-top: 16px;
+  }
+
+</style>
